3098bea9d072c5a5ba350a26260e5c5da11bd6cf
Qusai Obaidollah <qusaiobaidollah@gmail.com>
2026-02-16 19:23:07 +0300

    Update website design and content

--- assets/js/Team.js
+++ assets/js/Team.js
@@ -1,6 +1,15 @@
  document.addEventListener("contextmenu", function(e) {
   e.preventDefault();
 
+document.addEventListener("keydown", function (e) {
+  if (
+    (e.ctrlKey && (e.key === "s" || e.key === "u")) ||
+    e.key === "F12"
+  ) {
+    e.preventDefault();
+  }
+});
 
   
 });
+
